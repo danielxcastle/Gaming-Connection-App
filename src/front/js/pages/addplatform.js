c0e21b63121e6bf8b83382a5ba2516b9f6de790e
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-06 23:19:19 +0000

    fixed\y for platforms

--- src/front/js/pages/addplatform.js
+++ src/front/js/pages/addplatform.js
@@ -32,8 +32,77 @@ export const AddPlatform = () => {
         }
     };
     return (
-        <div className="container">
-            
+    <div>
+        <h2>Add User Platform</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <div>
+                <h3>Choose Platform(s):</h3>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="xbox"
+                        checked={formData.platformNames.xbox}
+                        onChange={handleInputChange}
+                    />
+                    Xbox
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="playstation"
+                        checked={formData.platformNames.playstation}
+                        onChange={handleInputChange}
+                    />
+                    Playstation
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="nintendo"
+                        checked={formData.platformNames.nintendo}
+                        onChange={handleInputChange}
+                    />
+                    Nintendo
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="discord"
+                        checked={formData.platformNames.discord}
+                        onChange={handleInputChange}
+                    />
+                    Discord
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="steam"
+                        checked={formData.platformNames.steam}
+                        onChange={handleInputChange}
+                    />
+                    Steam
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="battleNet"
+                        checked={formData.platformNames.battleNet}
+                        onChange={handleInputChange}
+                    />
+                    Battle.net
+                </label>
+            </div>
+            <button type="submit">Add Platform</button>
+        </form>
         </div>
     )
 
