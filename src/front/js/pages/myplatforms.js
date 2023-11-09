b951bb523c378c94b65a6af3d1fe23c44acd9f9e
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-09 22:13:47 +0000

    delete in myplatforms

--- src/front/js/pages/myplatforms.js
+++ src/front/js/pages/myplatforms.js
@@ -1,33 +1,48 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../store/appContext';
+import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
 
 export const MyPlatforms = () => {
     const { store, actions } = useContext(Context);
     const [userPlatforms, setUserPlatforms] = useState([]);
+    const navigate = useNavigate(); // Initialize the useNavigate hook
+
     const getPlatformIcon = platformName => {
-        // Log the platform name to check what's being received
-        console.log('Platform Name:', platformName);
-    
         switch (platformName) {
-            case 'Xbox':
-                return 'fab fa-xbox'; // Replace with the actual Font Awesome class for Xbox
-            case 'PlayStation':
-                return 'fab fa-playstation'; // Replace with the actual Font Awesome class for PlayStation
-            case 'Nintendo':
-                return 'fas fa-gamepad'; // Replace with the actual Font Awesome class for Nintendo
-            case 'Discord':
-                return 'fab fa-discord'; // Replace with the actual Font Awesome class for Discord
-            case 'Steam':
-                return 'fab fa-steam'; // Replace with the actual Font Awesome class for Steam
-            case 'Battle.net':
-                return 'fab fa-battle-net'; // Replace with the actual Font Awesome class for Battle.net
+            case 'xbox':
+                return 'fab fa-xbox';
+            case 'playStation':
+                return 'fab fa-playstation';
+            case 'nintendo':
+                return 'fas fa-gamepad';
+            case 'discord':
+                return 'fab fa-discord';
+            case 'steam':
+                return 'fab fa-steam';
+            case 'battle.net':
+                return 'fab fa-battle-net';
             default:
-                return ''; // If the platform name doesn't match any case, return an empty string
+                return '';
         }
     };
 
-   
-    
+    const handleDeletePlatform = async (platformId) => {
+        try {
+            const response = await actions.deletePlatform(platformId);
+            if (response && response.message === "Platform deleted successfully") {
+                const updatedPlatforms = await actions.getMyPlatforms();
+                setUserPlatforms(updatedPlatforms);
+                
+                // Navigate to "/profile" after successful deletion
+                navigate('/profile');
+            } else {
+                console.error("Failed to delete platform.");
+            }
+        } catch (error) {
+            console.error('Error deleting platform:', error);
+        }
+    };
+
     useEffect(() => {
         const fetchUserPlatforms = async () => {
             try {
@@ -39,17 +54,15 @@ export const MyPlatforms = () => {
                     });
                     if (response.ok) {
                         const contentType = response.headers.get('content-type');
-                        console.log('Content Type:', contentType);
 
                         if (contentType && contentType.includes('application/json')) {
                             const userPlatformsData = await response.json();
-                            console.log('User Platforms Data:', userPlatformsData);
                             setUserPlatforms(userPlatformsData.platforms);
                         } else {
                             console.error('Response is not in JSON format:', contentType);
                         }
                     } else {
-                        const errorData = await response.text(); 
+                        const errorData = await response.text();
                         console.error('Failed to fetch user platforms:', errorData);
                     }
                 }
@@ -60,8 +73,6 @@ export const MyPlatforms = () => {
 
         fetchUserPlatforms();
     }, [store && store.accessToken, store && store.user && store.user.id, store, actions]);
-    console.log('User Platforms:', userPlatforms);
-
 
     return (
         <div>
@@ -72,6 +83,9 @@ export const MyPlatforms = () => {
                         <i className={getPlatformIcon(platform.platform_name)}></i>
                         <span>{platform.platform_name}</span>
                         <span> - Username: {platform.username}</span>
+                        <button onClick={() => handleDeletePlatform(platform.id)}>
+                            <i className='fab fa-trash'></i>
+                        </button>
                     </li>
                 ))}
             </ul>
